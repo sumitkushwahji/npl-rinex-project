855b67ea8c685b0766ba40593026aef8c35ab500
sumitkushwahji <sumitkushwahji@gmail.com>
2024-07-23 16:52:10 +0530

    new file added

--- Receiver_class_new.py
+++ Receiver_class_new.py
@@ -594,9 +594,7 @@ def export_irnss_data_to_file(receiver, filename):
 # Example usage
 receiver = Receiver()
 
-receiver.import_data(
-    "H:\\NavIC_Tri_band_Data\\ITBR2910\\ITBR2910.23O"
-)  # Update the filepath accordingly
+receiver.import_data("ACCO0020.24O")  # Update the filepath accordingly
 
 export_irnss_data_to_file(receiver, "irnss_observation_data.txt")
 
